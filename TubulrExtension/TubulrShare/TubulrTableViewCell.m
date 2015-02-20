d1edc696549eb0b21aa7c893df04628e2bc3fe7a
spacedrabbit <louis.tur@gmail.com>
2015-02-20 14:34:28 -0500

    Adds uicolor category to standardize UI

--- TubulrExtension/TubulrShare/TubulrTableViewCell.m
+++ TubulrExtension/TubulrShare/TubulrTableViewCell.m
@@ -7,7 +7,11 @@
 //
 
 #import "TubulrTableViewCell.h"
+#import "UIColor+TubulrColors.h"
+
 #import <FontAwesomeKit/FAKFontAwesome.h>
+
+CGFloat const kIconSizes = 40.0;
 
 @interface TubulrTableViewCell () <MCSwipeTableViewCellDelegate>
 
@@ -28,33 +32,34 @@
     // TODO: Create UIColor Category
     
     [self setSelectionStyle:UITableViewCellSelectionStyleNone];
-    self.backgroundColor = [UIColor colorWithRed:42.0/255.00 green:42.0/255.00 blue:42.0/255.00 alpha:1.0];
-    [self.textLabel setTextColor:[UIColor colorWithRed:170.0/255.0 green:170.0/255.0 blue:170.0/255.0 alpha:1.0]];
+    self.backgroundColor = [UIColor srl_mainBackgroundDarkGrayColor];
+    [self.textLabel setTextColor:[UIColor srl_textFieldLightGrayColor]];
     [self.textLabel setFont:[UIFont fontWithName:@"Helvetica-Neue" size:18.0]];
     [self.layer setCornerRadius:10.0];
     [self.layer setMasksToBounds:YES]; //clips the highlighted state to bounds of the cell
     
-    FAKFontAwesome * heartIcon = [FAKFontAwesome heartIconWithSize:24.0];
-    FAKFontAwesome * watchLaterIcon = [FAKFontAwesome closeIconWithSize:24.0];
-    [heartIcon addAttribute:NSForegroundColorAttributeName value:[UIColor whiteColor]];
+    FAKFontAwesome * heartIcon      = [FAKFontAwesome heartIconWithSize:kIconSizes];
+    FAKFontAwesome * watchLaterIcon = [FAKFontAwesome closeIconWithSize:kIconSizes];
+    
+    [heartIcon      addAttribute:NSForegroundColorAttributeName value:[UIColor whiteColor]];
     [watchLaterIcon addAttribute:NSForegroundColorAttributeName value:[UIColor whiteColor]];
-    [watchLaterIcon addAttribute:NSBackgroundColorAttributeName value:[UIColor colorWithRed:93.0/255.0 green:192.0/255.0 blue:210.0/255.0 alpha:1.0]];
+    [watchLaterIcon addAttribute:NSBackgroundColorAttributeName value:[UIColor srl_watchBlueColor]];
 
     
-    UIImage * heartIconImage = [heartIcon imageWithSize:CGSizeMake(40, 40)];
-    UIImage * watchLaterIconImage = [watchLaterIcon imageWithSize:CGSizeMake(40, 40)];
+    UIImage * heartIconImage        = [heartIcon        imageWithSize:CGSizeMake(40, 40)];
+    UIImage * watchLaterIconImage   = [watchLaterIcon   imageWithSize:CGSizeMake(40, 40)];
     
     UIImageView * heartImageView = [[UIImageView alloc] initWithImage:heartIconImage];
     UIImageView * watchLaterImageView = [[UIImageView alloc] initWithImage:watchLaterIconImage];
     
-    [self setShouldDrag:YES];
-    [self setShouldAnimateIcons:YES];
-    [self setDamping:.5];
-    [self setDefaultColor:[UIColor clearColor]];
-    [self setColor1:[UIColor colorWithRed:250.0/255.0 green:127.0/255.0 blue:146.0/255.0 alpha:1.0]];
-    [self setColor2:[UIColor colorWithRed:93.0/255.0 green:192.0/255.0 blue:210.0/255.0 alpha:1.0]];
+    [self setShouldDrag         :YES];
+    [self setShouldAnimateIcons :YES];
+    [self setDamping            :.8 ];
+    [self setDefaultColor       :[UIColor clearColor]];
+    [self setColor1:[UIColor srl_heartPinkColor]];
+    [self setColor2:[UIColor srl_watchBlueColor]];
     [self setSwipeGestureWithView:heartImageView
-                            color:[UIColor colorWithRed:250.0/255.0 green:127.0/255.0 blue:146.0/255.0 alpha:1.0]
+                            color:[UIColor srl_heartPinkColor]
                              mode:MCSwipeTableViewCellModeSwitch
                             state:MCSwipeTableViewCellState1
                   completionBlock:^(MCSwipeTableViewCell *cell, MCSwipeTableViewCellState state, MCSwipeTableViewCellMode mode)
@@ -63,7 +68,7 @@
         
     }];
     [self setSwipeGestureWithView:watchLaterImageView
-                            color:[UIColor colorWithRed:93.0/255.0 green:192.0/255.0 blue:210.0/255.0 alpha:1.0]
+                            color:[UIColor srl_watchBlueColor]
                              mode:MCSwipeTableViewCellModeSwitch
                             state:MCSwipeTableViewCellState3
                   completionBlock:^(MCSwipeTableViewCell *cell, MCSwipeTableViewCellState state, MCSwipeTableViewCellMode mode)
